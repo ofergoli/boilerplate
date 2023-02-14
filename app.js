5265123e5f78a735c0f546694dcbeff06d26438e
Ofer Golibroda <ofer.golibroda@forter.com>
2023-02-14 15:05:33 +0200

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) =>  {
+   console.log('User Connected...');
+
+   socket.on('message', (msg) => {
+      console.log(msg);
+      socket.emit("message", "hello world from server");
+   });
+
+   socket.on('disconnect', () => {
+      console.log('User Disconnected...');
+   });
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
